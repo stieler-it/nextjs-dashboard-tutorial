031c1d9e6da8bbe8becfa660a4ac44d58fa363bf
Michael Cornel <michael@stieler.it>
2024-02-29 13:46:58 +0000

    Suspense for cards

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -61,6 +61,8 @@ export async function fetchLatestInvoices() {
 export async function fetchCardData() {
   noStore();
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
