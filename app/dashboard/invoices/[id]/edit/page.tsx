48e50e31ac28874e4d449e556ae870e5fa088b09
Michael Cornel <michael@stieler.it>
2024-03-03 17:16:59 +0000

    Add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,14 +2,22 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
  
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const id = params.id;
+  return {
+    title: 'Edit invoice ' + id,
+  }
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-  
+
   if (!invoice) {
     notFound();
   }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,13 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import React from 'react';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Dashboard',
+  },
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
